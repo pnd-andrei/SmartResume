fa382f208719e1e073c90bd65dde90d109147e50
Andrei Piscoran <aosxap@gmail.com>
2024-07-11 15:06:33 +0300

    improved api endpoints

--- resumeCentralPro/resumeApi/serializers.py
+++ resumeCentralPro/resumeApi/serializers.py
@@ -4,4 +4,4 @@ from .models import Resume
 class ResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resume
-        fields = ["description", "cv_url", "timestamp", "processed"]
+        fields = '__all__'
--- resumeCentralPro/resumeApi/views/resume.py
+++ resumeCentralPro/resumeApi/views/resume.py
@@ -5,8 +5,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from ..models import Resume
 from ..serializers import ResumeSerializer
-
+from ..forms import ResumeForm
 from datetime import datetime
+from django.shortcuts import render, redirect
 
 class ResumeApiView(APIView):
     # add permission to check if user is authenticated
@@ -15,35 +16,27 @@ class ResumeApiView(APIView):
     # 1. List all
     def get(self, request, *args, **kwargs):
         '''
-        List all the todo items for given requested user
+        List all the resume items for given requested user
         '''        
-        
         resumes = Resume.objects 
-        print(resumes)
 
         if resumes:
             serializer = ResumeSerializer(resumes, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
+            form = ResumeForm()
+            return render(request, 'add_resume.html', {'form': form, 'logs': serializer.data})
+
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     # 2. Create
     def post(self, request, *args, **kwargs):
-        '''
-        Create the Todo with given todo data
-        '''
-        data = {
-            'description': request.data.get('description'), 
-            'processed': False,
-            'timestamp':  f"{datetime.now()}",
-            'cv_url': request.data.get('cv_url')
-        }
+        form = ResumeForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            instance = form.save()  # Save form data and get the instance
+            print(f"Resume saved: {instance}")  # Debug output
+            return Response({'message': 'Resume created successfully'}, status=status.HTTP_201_CREATED)
+        
+        print(form.errors)  # Debug output for form errors
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = ResumeSerializer(data=data)
 
-        if serializer.is_valid():
-            print("save")
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
