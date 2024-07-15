77fbd5d49a4be580b6ff328e793d176b07e335b4
Andrei Piscoran <aosxap@gmail.com>
2024-07-15 11:52:46 +0300

    updated gitignore and small refactorings

--- resumecentral/src/tasks.py
+++ resumecentral/src/tasks.py
@@ -7,46 +7,56 @@ run(from terminal) with "invoke <command>"
 from invoke import task
 import platform
 
+
 @task
 def run(c):
     c.run("python manage.py runserver")
+
 
 @task
 def runfirst(c):
     syncdb(c)
     run(c)
 
+
 @task
 def migrate(c):
     c.run("python manage.py makemigrations")
     c.run("python manage.py migrate")
 
+
 @task
 def flush(c):
-    if platform.system() == 'Windows':  
+    if platform.system() == "Windows":
         c.run("del /Q .\\media\\*")
     else:  # Unix-like system
         c.run("rm -rf ./media/*")
-    
+
     c.run("python manage.py flush --noinput")
     migrate(c)
+
 
 @task
 def syncdb(c):
     c.run("python manage.py migrate --run-syncdb")
 
+
 @task
 def delete_pycache(c):
     system = platform.system()
-    
-    if system == 'Windows':
+
+    if system == "Windows":
         delete_pycache_windows(c)
     else:
         delete_pycache_unix(c)
 
+
 def delete_pycache_windows(c):
     c.run("del /s /q __pycache__\\*")  # Delete __pycache__ directories recursively
-    c.run("for /d %x in (*) do del /s /q %x\\__pycache__\\*")  # Delete __pycache__ directories in subdirectories recursively
+    c.run(
+        "for /d %x in (*) do del /s /q %x\\__pycache__\\*"
+    )  # Delete __pycache__ directories in subdirectories recursively
+
 
 def delete_pycache_unix(c):
     c.run("rm -rf ./*/__pycache__")
