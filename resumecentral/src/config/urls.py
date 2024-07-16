66ce52a3338ab7e9b0c90a6e57c94f17901b98ec
Andrei <panduru.andrei04@gmail.com>
2024-07-16 11:45:53 +0300

    modificari

--- resumecentral/src/config/urls.py
+++ resumecentral/src/config/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from api import urls as resume_urls
 from django.contrib import admin
 from django.urls import include, path
-
-from api import urls as resume_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
