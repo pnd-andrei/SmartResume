8c6818c93d40f5c8bf112930123e3ebf1320a7f6
Andrei Piscoran <aosxap@gmail.com>
2024-07-18 10:51:52 +0300

    refactored auth_templates

--- resumecentral/src/api/forms/register_form.py
+++ resumecentral/src/api/forms/register_form.py
@@ -9,3 +9,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
