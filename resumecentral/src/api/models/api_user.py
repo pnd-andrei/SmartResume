2fed9781949d25913dbaffaf3fba2492bdd8b76e
Andrei Piscoran <aosxap@gmail.com>
2024-07-18 20:58:33 +0300

    cleaning

--- resumecentral/src/api/models/api_user.py
+++ resumecentral/src/api/models/api_user.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class ApiUser(AbstractUser):
+    email = models.EmailField(unique=True) #make mail unique
     temporary_field = models.CharField(max_length=100)
