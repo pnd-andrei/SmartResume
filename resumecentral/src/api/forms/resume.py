c7981b52ac4f689c74045427d4fb741672551e24
Andrei Piscoran <aosxap@gmail.com>
2024-07-18 09:06:16 +0300

    refactored forms

--- resumecentral/src/api/forms/resume.py
+++ resumecentral/src/api/forms/resume.py
@@ -2,9 +2,8 @@ from django import forms
 
 from api.models.resume_model import Resume
 
-# creating a form
+
 class ResumeForm(forms.ModelForm):
-    # specify the name of model to use
     class Meta:
         model = Resume
         fields = "__all__"
