967f961be071757c558e1969a3d6ad520e92060f
Andrei Piscoran <aosxap@gmail.com>
2024-07-18 09:07:06 +0300

    isort

--- resumecentral/src/api/urls.py
+++ resumecentral/src/api/urls.py
@@ -1,12 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from api.views.register_views.resume_list_view import ResumeApiView
-from api.views.register_views.resume_detail_view import IndividualResumeApiView
-
-from django.conf.urls.static import static
-from django.conf import settings
-
 from api.views.register_views.resume_delete_view import DeleteResumeApiView
+from api.views.register_views.resume_detail_view import IndividualResumeApiView
+from api.views.register_views.resume_list_view import ResumeApiView
 
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
