32ed6d6e086d2a4a9547fa0c61154d7fcc9a1a30
Andrei Piscoran <aosxap@gmail.com>
2024-07-21 13:05:21 +0300

    refactored authentication

--- resumecentral/src/api/modules/hasher.py
+++ resumecentral/src/api/modules/hasher.py
@@ -1,6 +1,6 @@
 import hashlib
 import pickle
-
+from uuid import uuid4
 
 def object_to_bytes(obj):
     return pickle.dumps(obj)
@@ -9,3 +9,7 @@ def compute_hash(obj):
     byte_representation = object_to_bytes(obj)
     hash_object = hashlib.sha256(byte_representation)
     return hash_object.hexdigest()
+
+def secret_key():
+    return uuid4()    
+
