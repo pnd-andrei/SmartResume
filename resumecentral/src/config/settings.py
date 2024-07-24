d626bfa749ae33ecb9e701204f0133bd48eae5d6
Andrei Piscoran <aosxap@gmail.com>
2024-07-24 11:28:15 +0300

    updated for heroku

--- resumecentral/src/config/settings.py
+++ resumecentral/src/config/settings.py
@@ -142,5 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "api.ApiUser"
 
-import django_heroku
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
