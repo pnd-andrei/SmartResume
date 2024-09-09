bd8b19c7c744536b4fdb78b6402b153aa5c0aa5c
Andrei <panduru.andrei04@gmail.com>
2024-09-09 20:50:17 +0300

    modificari

--- resumecentral/src/controllers/delete_user.py
+++ resumecentral/src/controllers/delete_user.py
@@ -1,3 +1,4 @@
+from api.models.user import ApiUser
 import os
 import sys
 
@@ -11,12 +12,11 @@ sys.path.append(src_dir)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-from api.models.user import ApiUser  #
 
 user_id = 2
 try:
     user = ApiUser.objects.get(id=user_id)
     user.delete()
     print(f"User with ID {user_id} has been deleted.")
-except User.DoesNotExist:
+except user.DoesNotExist:
     print(f"User with ID {user_id} does not exist.")
